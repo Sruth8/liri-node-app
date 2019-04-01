fbfc7814cf1da8eb63b51febe227e0e60ee6c648
Shelisha Rutherford <starshipazul@yahoo.com>
2019-03-31 23:33:11 -0400

    HW10

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # liri-node-app
 HW10
+ This assignment is about building a app that acts like SIRI. It basically responds to the users input.
---
+++ liri.js
@@ -0,0 +1,40 @@
+var axios = require("axios");
+var spotify = require("node-spotify-api");
+
+var fs = require("fs");
+
+
+var Music = function() {
+
+this.divider = "\n----------------------------\n\n";
+
+
+this.findBands = function(bands) {
+
+    var URL = 'https://api.spotify.com/v1/bands')
+
+
+    axios.get(URL).then(function(respose) {
+    
+    var jsonData = response.data;
+    var  
+    
+
+
+
+
+    }
+    )}
+
+
+
+
+
+
+
+
+
+
+
+
+}
