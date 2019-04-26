d6f3b2d83757024cf46ea1373a95b8c47bdf53e2
Shelisha Rutherford <starshipazul@yahoo.com>
2019-04-25 22:42:51 -0400

    work in progress

--- liri.js
+++ liri.js
@@ -1,30 +1,35 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
 var axios = require("axios");
 var spotify = require("node-spotify-api");
 
 var fs = require("fs");
 
+var spotify = new Spotify(keys.spotify);
 
-var Music = function() {
+var Music = function () {
 
-this.divider = "\n----------------------------\n\n";
-
-
-this.findBands = function(bands) {
-
-    var URL = 'https://api.spotify.com/v1/bands')
+    this.divider = "\n----------------------------\n\n";
 
 
-    axios.get(URL).then(function(respose) {
-    
-    var jsonData = response.data;
-    var  
-    
+    this.findBands = function (bands) {
+
+        var URL = 'https://api.spotify.com/v1/bands') {
+
+
+            axios.get(URL).then(function (respose) {
+
+                var jsonData = response.data;
+                var
 
 
 
 
+
+            }
+           )
     }
-    )}
 
 
 
