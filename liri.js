26a5e2b4474796253633058b116a158ec30abfc3
Shelisha Rutherford <starshipazul@yahoo.com>
2019-05-14 22:37:37 -0400

    added concert-this

--- liri.js
+++ liri.js
@@ -11,7 +11,7 @@ var Spotify = require("node-spotify-api");
 
 var spotify = new Spotify(keys.spotify);
 var request = require("request");
-
+var moment = require("moment"); // for the concert api
 // var spotify = new Spotify({
 
 //   });
@@ -31,7 +31,7 @@ var request = require("request");
 
 var input = process.argv;
 var command = input[2]; // this gets the users input
-var userdata = input[3];                      
+var userdata = input[3];
 // var userdata = "";
 
 //  for (i = 3; i < input.length; i++) {
@@ -43,33 +43,33 @@ var userdata = input[3];
 function spotifyJams(info) {
     if (info) {
 
-    info =  'The Sign by Ace of Base';  //info || '"The Sign" by Ace of Base'
-    
+        info = 'The Sign by Ace of Base';  //info || '"The Sign" by Ace of Base'
+
     }
     spotify.search({ type: 'track', query: info }, function (error, data) {
 
         if (error) {
-             console.log('Error occurred: ' + error);
-             return; 
+            console.log('Error occurred: ' + error);
+            return;
         }
 
-       // console.log(data);
-         var report = data.tracks.items;
+        // console.log(data);
+        var summary = data.tracks.items;
 
-        console.log("Artist(s): " + report[0].artists[0].name);
-        console.log("Song Name: " + report[0].name);
-        console.log("Preview Link: " + report[0].preview_url);
-        console.log("Album: " + report[0].album.name);
+        console.log("Artist(s): " + summary[0].artists[0].name);
+        console.log("Song Name: " + summary[0].name);
+        console.log("Preview Link: " + summary[0].preview_url);
+        console.log("Album: " + summary[0].album.name);
     })
 
- };
+};
 
 
 
 //  movie-this function
 // Watch levelOneOmdbInteractive.js video
 function omdbFlix(info) {
-           //axios.get("http://www.omdbapi.com/?i=&y=&plot=short&apikey=trilog")
+    //axios.get("http://www.omdbapi.com/?i=&y=&plot=short&apikey=trilogy")
     var queryUrl = "http://www.omdbapi.com/?t=" + info + "&y=&plot=short&apikey=trilogy";
 
     request(queryUrl, function (error, response, body) {
@@ -77,7 +77,7 @@ function omdbFlix(info) {
         if (info) {
             info = 'Mr Nobody';
         }
-            // 200= this error code means the reponse was sent and it is ok on the http level
+        // 200= this error code means the reponse was sent and it is ok on the http level
         if (!error && response.statusCode === 200) {
 
             console.log("Title: " + JSON.parse(body).Title);
@@ -94,11 +94,11 @@ function omdbFlix(info) {
 };
 
 // //  do-what-it-says function
-function liriSays(){
+function liriSays() {
 
     fs.readFile('random.txt', 'utf8', function (error, data) {
 
-        if(error){
+        if (error) {
             return console.log(error)
         };
 
@@ -107,8 +107,11 @@ function liriSays(){
         if (dataArr[0] === 'spotify-this-song') {
             spotifyJams(dataArr[1]);
 
-        } else if (dataArr[0] === 'movie-this'){
+        } else if (dataArr[0] === 'movie-this') {
             omdbFlix(dataArr[1]);
+
+        } else if (dataArr[0] === 'concert-this') {
+            concert(dataArr[1]);
 
         } else {
             console.log("What are you doing?")
@@ -117,8 +120,34 @@ function liriSays(){
     });
 
 };
+  //concert-this
+function concert(info) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + info + "/events?app_id=codingbootcamp";
 
-// //  Run liri.js
+    request(queryUrl, function (error, response) {
+        if (!info) {
+            info = "Purple Rain, Prince";
+
+        }
+
+        //var result = JSON.parse(body)[0];  // got this from youtube. this is suppose to formate the info in console.log
+        if (!error && response.statusCode === 200) {
+            console.log("City: " + result.venue.city);
+            console.log("Venue Name: " + result.venue.name);
+            console.log("Event Date: " + moment(result.datetime).format("mm/dd/yyyy")
+            );
+        }
+
+    });
+}
+
+function concert() {
+    fs.readFile("random.txt", "utf-8", function(error, data) {
+        console.log(error);
+    });
+}
+
+// //  Run liri.js  switches
 switch (command) {
 
 
@@ -138,9 +167,17 @@ switch (command) {
         };
         break;
 
-        case "do-what-it-says":
-            liriSays();
-            break;
+    case "do-what-it-says":
+        liriSays();
+        break;
+
+    case "concert-this":
+        if (userdata) {
+            concert(userdata);
+        } else {
+            concert("  ")
+        };
+        break;
 
     default:
         console.log("{Enter a command:  spotify-this-song, movie-this, do-what-it-says}");
